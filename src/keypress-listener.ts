90521022a204905d159206a5c2529b674d3ec0cc
Faizan Ali <faizanali04@hotmail.com>
2018-07-01 00:51:19 -0700

    feat: Backspace on query

--- src/keypress-listener.ts
+++ src/keypress-listener.ts
@@ -1,47 +1,54 @@
 import { initQuickwiki } from './index'
-import { ITerminal } from './types/terminal-kit'
 import { exitTerminal } from './utils'
 import store from './services/store'
 import { print } from './utils'
+import keys from './constants/keys'
 
-const keySequences = {
-    ctrlC: 'CTRL_C',
-    enter: 'ENTER',
-    esc: 'ESCAPE',
-    q: 'q'
-}
+const terminal = store.getTerminal()
 
-const exitCodes: string [] = [ keySequences.ctrlC, keySequences.esc ]
+const exitCodes: string [] = [ keys.ctrlC, keys.esc ]
 
+// Kind of static variable
+let isListening = false
 /**
  * Listens for keyboard events
  * Exits if any key in exitCodes is pressed since terminal-kit does not support Ctrl+C exits
  */
-export default function (terminal: ITerminal) {
-    let isQuery = false
-    let queryBuffer: string [] = []
-
-    terminal.grabInput(true)
-    // Start the keypress listener for the process
-    terminal.on('key', name => {
-        if (exitCodes.includes(name)) exitTerminal()
-        // If a query has been triggered by pressing 'q', collects all keystrokes to form a query that is submitted on 'Enter'
-        else if (isQuery) {
-            if (name === keySequences.enter) {
-                isQuery = false
-                // Absolutely crucial otherwise two menus will display
-                store.getMenu().abort()
-                initQuickwiki(queryBuffer.join(''))
-                queryBuffer = []
-            } else {
-                queryBuffer.push(name)
-                printQuery(queryBuffer.join(''))
+export default function () {
+    if (!isListening) {
+        isListening = true
+        let isQuery = false
+        let queryBuffer: string [] = []
+        console.log('INIT LISTENER')
+        isListening = true
+        terminal.grabInput(true)
+        // Start the keypress listener for the process
+        terminal.on('key', name => {
+            // Exits terminal if an exit command is given
+            if (exitCodes.includes(name)) exitTerminal()
+            // If a query has been triggered by pressing 'q', collects all keystrokes to form a query that is submitted on 'Enter'
+            else if (isQuery) {
+                if (name === keys.enter) {
+                    isQuery = false
+                    // Absolutely crucial otherwise two menus will display
+                    store.getMenu().abort()
+                    initQuickwiki(queryBuffer.join(''))
+                    queryBuffer = []
+                    // Literal backspace for typed query
+                } else if (name === keys.backspace) {
+                    delete queryBuffer[ queryBuffer.length - 1 ]
+                    printQuery(queryBuffer.join(''))
+                } else {
+                    queryBuffer.push(name)
+                    printQuery(queryBuffer.join(''))
+                }
+                // Triggers query
+            } else if (name === keys.q) {
+                isQuery = true
+                printQuery()
             }
-        } else if (name === keySequences.q) {
-            isQuery = true
-            printQuery()
-        }
-    })
+        })
+    }
 }
 
 const printQuery = (query = '') => {
